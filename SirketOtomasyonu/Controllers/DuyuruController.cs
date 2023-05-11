cb1f4bf7017caca72da2388e8b9a1ccd267b4f85
sametcekin <sametcekin@outlook.com.tr>
2023-05-11 21:16:32 +0300

    duyuru ıs added

---
+++ SirketOtomasyonu/Controllers/DuyuruController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SirketOtomasyonu.Data;
+using SirketOtomasyonu.Data.Entities;
+
+namespace SirketOtomasyonu.Controllers
+{
+    public class DuyuruController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DuyuruController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Duyuru
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Duyuru.ToListAsync());
+        }
+
+        // GET: Duyuru/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Duyuru == null)
+            {
+                return NotFound();
+            }
+
+            var duyuru = await _context.Duyuru
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (duyuru == null)
+            {
+                return NotFound();
+            }
+
+            return View(duyuru);
+        }
+
+        // GET: Duyuru/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Duyuru/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Baslik,Icerik,Aciklama,Tarih")] Duyuru duyuru)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(duyuru);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(duyuru);
+        }
+
+        // GET: Duyuru/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Duyuru == null)
+            {
+                return NotFound();
+            }
+
+            var duyuru = await _context.Duyuru.FindAsync(id);
+            if (duyuru == null)
+            {
+                return NotFound();
+            }
+            return View(duyuru);
+        }
+
+        // POST: Duyuru/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Baslik,Icerik,Aciklama,Tarih")] Duyuru duyuru)
+        {
+            if (id != duyuru.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(duyuru);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DuyuruExists(duyuru.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(duyuru);
+        }
+
+        // GET: Duyuru/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Duyuru == null)
+            {
+                return NotFound();
+            }
+
+            var duyuru = await _context.Duyuru
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (duyuru == null)
+            {
+                return NotFound();
+            }
+
+            return View(duyuru);
+        }
+
+        // POST: Duyuru/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Duyuru == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Duyuru'  is null.");
+            }
+            var duyuru = await _context.Duyuru.FindAsync(id);
+            if (duyuru != null)
+            {
+                _context.Duyuru.Remove(duyuru);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DuyuruExists(int id)
+        {
+            return _context.Duyuru.Any(e => e.Id == id);
+        }
+    }
+}
--- SirketOtomasyonu/Data/Entities/Duyuru.cs
+++ SirketOtomasyonu/Data/Entities/Duyuru.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
 
 namespace SirketOtomasyonu.Data.Entities
 {
     public class Duyuru
     {
         public int Id { get; set; }
+        [DisplayName("Başlık")]
         public string Baslik { get; set; }
+        [DisplayName("İçerik")]
         public string Icerik { get; set; }
+        [DisplayName("Açıklama")]
         public string Aciklama { get; set; }
         public DateTime Tarih { get; set; }
     }
