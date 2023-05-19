f17b3686ad23f7a971e2a97f1dba36e4701be3e3
sametcekin <sametcekin@outlook.com.tr>
2023-05-19 08:28:25 +0300

    jquery datatable is added

--- SirketOtomasyonu/Controllers/IletisimController.cs
+++ SirketOtomasyonu/Controllers/IletisimController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SirketOtomasyonu.Data;
 using SirketOtomasyonu.Data.Entities;
+using SirketOtomasyonu.Models.Iletisim;
 
 namespace SirketOtomasyonu.Controllers
 {
@@ -15,7 +17,34 @@ namespace SirketOtomasyonu.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index(string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+
+            var iletisimListe = _context.Iletisim.AsQueryable();
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                iletisimListe = iletisimListe.Where(x => x.AdiSoyadi.Contains(searchString)
+                || x.Email.Contains(searchString) || x.Baslik.Contains(searchString));
+            }
+            var model = new List<IletisimViewModel>();
+            foreach (var item in await iletisimListe.ToListAsync())
+            {
+                model.Add(new IletisimViewModel
+                {
+                    Id = item.Id,
+                    AdiSoyadi = item.AdiSoyadi,
+                    Baslik = item.Baslik,
+                    Email = item.Email,
+                    Mesaj = item.Mesaj,
+                    Tarih = item.Tarih,
+                });
+            }
+            return View(model);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
@@ -32,6 +61,54 @@ namespace SirketOtomasyonu.Controllers
                 return RedirectToAction("Index");
             }
             return View(iletisim);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Iletisim == null)
+            {
+                return NotFound();
+            }
+
+            var iletisim = await _context.Iletisim
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (iletisim == null)
+            {
+                return NotFound();
+            }
+            var model = new IletisimViewModel
+            {
+                Id = iletisim.Id,
+                AdiSoyadi = iletisim.AdiSoyadi,
+                Baslik = iletisim.Baslik,
+                Email = iletisim.Email,
+                Mesaj = iletisim.Mesaj,
+                Tarih = iletisim.Tarih,
+            };
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Iletisim == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Iletisim'  is null.");
+            }
+            var iletisim = await _context.Iletisim.FindAsync(id);
+            if (iletisim != null)
+            {
+                _context.Iletisim.Remove(iletisim);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool IletisimExists(int id)
+        {
+            return _context.Iletisim.Any(e => e.Id == id);
         }
     }
 }
--- SirketOtomasyonu/Models/Iletisim/IletisimCreateViewModel.cs
+++ SirketOtomasyonu/Models/Iletisim/IletisimCreateViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using System.ComponentModel;
 
-namespace SirketOtomasyonu.Data.Entities
+namespace SirketOtomasyonu.Models.Iletisim
 {
-    public class Iletisim
+    public class IletisimCreateViewModel
     {
         public int Id { get; set; }
+        [DisplayName("Adı Soyadı")]
         public string AdiSoyadi { get; set; }
         public string Email { get; set; }
+        [DisplayName("Başlık")]
         public string Baslik { get; set; }
         public string Mesaj { get; set; }
         public DateTime Tarih { get; set; }
